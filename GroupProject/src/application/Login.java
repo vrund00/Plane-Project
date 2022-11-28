57b7e86aaf1dc4109d296f3d57cf2ec5fe577035
vrundpatel <vrundpatel@vrunds-mbp.attlocal.net>
2022-11-27 23:00:43 -0500

    .

--- GroupProject/src/application/Login.java
+++ GroupProject/src/application/Login.java
@@ -1,5 +1,21 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Login {
+	
+	@FXML
+	public Label statusID;
+	
+	@FXML
+	
+	public TextField usernameID;
+	
+	@FXML
+	
+	public TextField passwordID;
+	
 
 }
