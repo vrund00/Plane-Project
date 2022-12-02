d1caf92db43c8d61909e2585b1139f5989995365
vrundpatel <vrundpatel@vrunds-mbp.wifi.gsu.edu>
2022-12-01 19:46:04 -0500

    Deleted all of Vrund files and tried again

--- GroupProject/src/GUI/Main.java
+++ GroupProject/src/GUI/Main.java
@@ -1,28 +1,22 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	public static void main(String[] args) {
 		launch(args);
-		
 	}
 	
 	@Override
 	
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World");
-		
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("NewAccountScene"));
 		primaryStage.show();
 	}
-	
-	
 
 }
