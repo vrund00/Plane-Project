0d91c2ee0cb01cea0930f1a60454fa73b1fcef4d
Jzash <Jzash@10.250.93.239>
2022-12-01 20:41:55 -0500

    ...

--- GroupProject/src/GUI/Main.java
+++ GroupProject/src/GUI/Main.java
@@ -3,6 +3,8 @@ package GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,9 +19,10 @@ public class Main extends Application {
 	
 	@Override
 	
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hello World");
-		
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("SearchFlights.fxml"));
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(new Scene(root, 800,800));
 		primaryStage.show();
 	}
 	
