9283de2d23e7a486ef7f7d556b2390ba8805aa7d
Jzash <Jzash@10.1.190.234>
2022-12-05 13:34:46 -0500

    Merge branch 'master' of https://github.com/vrund00/Plane-Project.git

--- GroupProject/src/GUI/NewAccountController.java
+++ GroupProject/src/GUI/NewAccountController.java
@@ -1,6 +1,10 @@
 package GUI;
 
 import java.awt.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,16 +13,68 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 //import javafx.event.ActionEvent;
 
 public class NewAccountController {
 	
-	@FXML
-	Button cancelButton;
-	
     @FXML
-    Button createNewUser;
+    private TextField adminIDTF;
+
+    @FXML
+    private TextField adminTF;
+
+    @FXML
+    private TextField answer1TF;
+
+    @FXML
+    private TextField answer2TF;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField cityTF;
+
+    @FXML
+    private Button createNewUser;
+
+    @FXML
+    private TextField emailTF;
+
+    @FXML
+    private TextField firstNameTF;
+
+    @FXML
+    private TextField lastNameTF;
+
+    @FXML
+    private TextField passwordTF;
+
+    @FXML
+    private TextField ssnTF;
+
+    @FXML
+    private TextField stateTF;
+
+    @FXML
+    private TextField streetTF;
+
+    @FXML
+    private TextField usernameTF;
+
+    @FXML
+    private TextField zipcodeTF;
 	
+public static Connection getConnection() throws SQLException{
+		
+		String connectionString = "jdbc:sqlserver://flightapp.database.windows.net:1433;database=CIS Application Project;user=vrund00@flightapp;password={Nehalp1974*};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+				
+				Connection connection = DriverManager.getConnection(connectionString);
+				System.out.println("Connection Established");
+				return connection;
+
+}    
 	
 	public void cancelButton() throws Exception{
 		Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
@@ -27,14 +83,45 @@ public class NewAccountController {
 	}
 	
 	// need to add update sql database with info line not only go back to login
-	public void createNewUser() throws Exception{
+	/*public void createNewUser() throws Exception{
 		Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
 		Stage window = (Stage)createNewUser.getScene().getWindow();
 		window.setScene(new Scene(root, 600, 600));
-	
 	}
-		
+	*/
+	
+	public void insertUserInfo()throws Exception {
+		try {
+			Connection con = getConnection();
+			
+			PreparedStatement ps= con.prepareStatement("insert into UserInfo(firstName, lastName, street, city, state, zipcode,email, ssn, username, password, answer1, answer2, admin,adminID) "
+					+ "											values(?,?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?,?)");
+			
+			ps.setString(1, firstNameTF.getText());
+			ps.setString(2, lastNameTF.getText());
+			ps.setString(3, streetTF.getText());
+			ps.setString(4, cityTF.getText());
+			ps.setString(5, stateTF.getText());
+			ps.setString(6, zipcodeTF.getText());
+			ps.setString(7, emailTF.getText());
+			ps.setString(8, ssnTF.getText());
+			ps.setString(9, usernameTF.getText());
+			ps.setString(10, passwordTF.getText());
+			ps.setString(11, answer1TF.getText());
+			ps.setString(12, answer2TF.getText());
+			ps.setString(13, adminTF.getText());
+			ps.setString(14, adminIDTF.getText());
+			ps.executeUpdate();
+			
+			
+			
+		}catch(Exception e) {System.out.println(e);}
+		finally {System.out.println("Function completed");};
+	}
+	
+	
 		
 }
+
 
 
